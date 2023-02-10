9fe28c908442dafe35469d8fdba5cd54a36575c5
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-02-09 22:32:43 -0800

    added more code examples to the index.js file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log("--------------------------------- Var And Let Keywords--------------------------------")
 var x =33;
 console.log(x)
 var add = 333+32;
@@ -11,6 +12,18 @@ console.log("number of hens "+hens)
 console.log(roosters)
 
 //Unary operators
+console.log("--------------------------------- Unary Operator --------------------------------")
 let scores =2
 scores++
 console.log(scores)
+scores--;
+console.log("score : "+scores)
+
+//Const keyword
+//the value of constant can not be changed 
+console.log("--------------------------------- Const Keyword--------------------------------")
+const finalScore =233;
+console.log("final Score : "+finalScore)
+const dayInTheWeek =7;
+const monthsInTheYear =12;
+
