e467c1ced9d31fc3530ee72f3a5c89cf92086996
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-02-09 22:54:09 -0800

    added modeldata.js file and some code example in it

---
+++ modeldata.js
@@ -0,0 +1,33 @@
+console.log("--------------------------------- Booleans --------------------------------")
+//True or False
+let isLogin = true;
+let gameOver = false;
+let isDied =true;
+
+console.log("is login + "+isLogin)
+console.log("is Game Over + "+gameOver)
+console.log("is Died + "+isDied)
+
+console.log("--------------------------------- Strings --------------------------------")
+let firstName ="abdul hasib"
+let lastName ='yousufzai'
+console.log(firstName)
+console.log(lastName)
+let fullName = firstName+" "+lastName
+console.log(fullName)
+
+let fullNameLength = fullName.length
+console.log("Full Name length: "+fullNameLength)
+console.log(fullName[0])
+console.log(fullName[8])
+
+console.log("--------------------------------- String Methods --------------------------------")
+console.log(fullName.toUpperCase())
+console.log(fullName.toLowerCase())
+console.log(fullName.trim())
+console.log(firstName.toUpperCase().trim())
+
+let text ="my dummy text "
+let indexOfDummy = text.indexOf("dummy")
+console.log(indexOfDummy)
+
