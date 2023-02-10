dd7c00223b4566429d11827d5270613e9610362d
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-02-09 22:25:29 -0800

    added more code examples to the index.js file

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ var modulas =3%2;
 var demo = "test"
 var nan = 0/0
 console.log(nan)
+let hens =4
+let roosters =4
+console.log("number of hens "+hens)
+console.log(roosters)
+
+//Unary operators
+let scores =2
+scores++
+console.log(scores)
