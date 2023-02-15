8e665ab3c664ef394a9215c00638aca737217934
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-02-14 21:39:42 -0800

    added more code examples to the controlinglogic.js file

--- controlinglogic.js
+++ controlinglogic.js
@@ -1,1 +1,11 @@
 console.log("--------------------------------- Comparasion Operators --------------------------------")
+console.log(44<2.4);
+console.log("string".length<="String!".length);
+
+console.log("--------------------------------- == vs === --------------------------------")
+console.log(false ==false);
+console.log('7'==7); //true
+console.log('7'===7);//false
+
+console.log("--------------------------------- IF statement --------------------------------")
+
