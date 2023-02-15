efad99b2469610b8c2264535af9c748966f621f7
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-02-14 21:59:08 -0800

    added switch  statement code examples to the controlinglogic.js file

--- controlinglogic.js
+++ controlinglogic.js
@@ -34,3 +34,17 @@ if(rating ===4){
 }else{
     console.log("invalid rating")
 }
+
+console.log("--------------------------------- Switch Statment --------------------------------")
+let day =3
+switch(day){
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    default:
+        console.log("invalid day")
+
+}
