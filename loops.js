dba72c867adce0f2fd2101768b973adac73957a7
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-02-18 14:36:40 -0800

    added nested loop code examples to the loops.js file

--- loops.js
+++ loops.js
@@ -19,3 +19,31 @@ console.log("--------------------------------- infinite loops ------------------
 //     console.log(k)
 // }
 
+console.log("--------------------------------- for loop and arrays of object --------------------------------")
+let students = [
+    {firstName:"abdul Hasib",
+marks:23},
+{
+    firstName:"noman",
+    marks:32
+}
+]
+
+for(let items =0;items<students.length;items++){
+    console.log(students[items])
+    console.log(students[items].firstName)
+}
+
+console.log("--------------------------------- nested loops --------------------------------")
+let gameBoard =[
+    [2233,3434,55],
+    [233,43,54],
+    [3423,43,55]
+]
+
+for( let game1 =0;game1<gameBoard.length;game1++){
+    let items = gameBoard[game1]
+    for(let game2=0;game2<items.length;game2++){
+        console.log(items[game2])
+    }
+}
