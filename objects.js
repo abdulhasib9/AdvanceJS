f1e9fef53423422347a20512d5d14e3914bd5e99
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-02-18 14:04:49 -0800

    added more code examples to the objects.js file

--- objects.js
+++ objects.js
@@ -4,3 +4,39 @@ const hasib ={
     "totalCalories":1230
 }
 console.log(hasib.totalMiles)
+
+const fitBitData ={
+    totalSteps:232,
+    totalMiles:324,
+    workoutThisWeek:'2 0ut of 7'
+}
+
+console.log(fitBitData.workoutThisWeek)
+
+const numbers ={
+    100:"one Hundred",
+    16: "sixteen"
+}
+console.log(numbers[100])
+console.log(numbers['100'])
+
+//adding and update data
+const userReviews ={
+
+}
+userReviews['QueenBee']=4.5;
+userReviews.newName=3.44;
+console.log(userReviews)
+userReviews.QueenBee +=0.5;
+console.log(userReviews)
+
+//objects inside objecs 
+let students ={
+    name: "abdul hasib",
+    scores:{
+        midterm:233,
+        final:44
+    }
+}
+let totalScores = students.scores.midterm +students.scores.final;
+console.log(totalScores)
