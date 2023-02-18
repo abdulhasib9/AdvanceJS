a0a4e183c7bd83443b6941abc1cc4ddf70449305
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-02-18 14:26:58 -0800

    added loops.js file and some code examples

--- loops.js
+++ loops.js
@@ -1,3 +1,21 @@
 for(let i =0;i<=10;i++){
     console.log(i)
 }
+
+//generating the table
+
+for (let j =0;j<=20;j++){
+    console.log(`${j} x ${j} = ${j*j}`)
+}
+//loop backward 
+console.log("Reverse Loop")
+for (let num=50;num>0;num--){
+    console.log(num)
+}
+
+console.log("--------------------------------- infinite loops --------------------------------")
+
+// for(let k =0;k!==21;k+=2){
+//     console.log(k)
+// }
+
