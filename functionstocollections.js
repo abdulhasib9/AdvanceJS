6e4bd69e04260c4e1af36519f38bb944eff7db72
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-02-19 19:50:14 -0800

    added map function code examples to the functionstocollections.js file

--- functionstocollections.js
+++ functionstocollections.js
@@ -8,3 +8,16 @@ function custom(x){
     console.log(x)
 }
 numbers.forEach(custom)
+
+
+//map function
+const doubleNumbers = numbers.map(function (num){
+    return num*2;
+})
+console.log(doubleNumbers)
+
+let words = ["asap","brb","lmk"]
+let wordsUpper = words.map(function(word){
+    return word.toUpperCase().split("").join(".")
+})
+console.log(wordsUpper)
