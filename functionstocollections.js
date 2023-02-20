bb319cd07f25b74ec6e5ea4f90ae866c74f881b5
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-02-19 20:13:11 -0800

    added more code examples to the functionstocollections.js file

--- functionstocollections.js
+++ functionstocollections.js
@@ -21,3 +21,43 @@ let wordsUpper = words.map(function(word){
     return word.toUpperCase().split("").join(".")
 })
 console.log(wordsUpper)
+
+console.log("------------------------------ Arrow Functions -----------------------------------")
+
+const arrowDemo = (x)=>{
+    return x*4
+}
+
+console.log(arrowDemo(3))
+
+//implicit return
+const squar = x=>x*x
+console.log(squar(3))
+
+console.log("------------------------------ Array.Find Method  -----------------------------------")
+
+const movies =[
+    "spider man",
+    "iron man",
+    "venom",
+    "bat man"
+]
+
+let isHaveMan = movies.find((m)=>{
+    return m.includes("man")
+})
+//find stops when the first match is found
+console.log(isHaveMan)
+
+console.log("------------------------------ Array.filter method  -----------------------------------")
+
+const containsMan = movies.filter(x=>x.includes("man"))
+console.log(containsMan)
+
+console.log("------------------------------ Some and Every method  -----------------------------------")
+
+const all3lets = movies.every(x=>x.length===3)
+console.log(all3lets)
+
+const some3lets = movies.some(x=>x.length===3)
+console.log(some3lets)
