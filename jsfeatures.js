20ed8c49c3ef91d5b262fcf431944c58ed17332b
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-02-19 21:17:40 -0800

    added jsfeatures.js file and some code examples

---
+++ jsfeatures.js
@@ -0,0 +1,9 @@
+console.log("------------------------------------ Spread in Array Literal --------------------------------")
+const animals1= ['fish','octopus']
+const animals2 =['dog','cat']
+let animalsList = [...animals1,...animals2]
+console.log(animalsList)
+
+let animals1Copy = [...animals1]
+console.log(animals1)
+console.log(animals1Copy)
