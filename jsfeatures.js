d22d6f4748cec23343ea41677a5f0c4ab688f2bb
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-02-20 21:38:07 -0800

    added spreading object code examples to the jsfeature.js file

--- jsfeatures.js
+++ jsfeatures.js
@@ -7,3 +7,17 @@ console.log(animalsList)
 let animals1Copy = [...animals1]
 console.log(animals1)
 console.log(animals1Copy)
+
+let  pets ={
+    type: "huskey",
+    ispet: true
+}
+
+let spreadPets ={
+    ...pets,
+    isAlive:true
+}
+console.log(spreadPets)
+
+let randoms = [..."Hello World".toUpperCase(),{...spreadPets}]
+console.log(randoms)
