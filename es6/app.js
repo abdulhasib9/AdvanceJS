409cc08886e8a52f08102ab7482613cbb8046ded
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-09-02 22:46:53 -0700

    added demo class and more code examples

--- es6/app.js
+++ es6/app.js
@@ -1,7 +1,34 @@
-function test(){
-    console.log(this)
+// // alert("working")
+
+// function test(){
+//     console.log(this)
+// }
+// test()
+// console.log('********* testing this keyword with arrow functions *********************')
+// var fn = ()=>console.log(this)
+// fn()
+
+// document.querySelector('button').addEventListener('click',test)
+
+//obejct litterals 
+let ageField  = 'Age'
+
+let person ={
+    [ageField]:25,
+    name :'test'
+
 }
-test()
-console.log('********* testing this keyword with arrow functions *********************')
-fn = ()=>console.log(this)
-fn()
+
+//console.log(person[ageField])
+
+//rest operator 
+function fn(...toadd){
+    let result =0
+    for(let i =0 ;i<toadd.length; i++){
+        result += toadd[i]
+    }
+   
+    return result
+}
+
+console.log(fn(23,444,555,3))
---
+++ es6/demo.js
@@ -0,0 +1,7 @@
+
+let name = 'abdul hasib'
+let template_literals = `
+    this is an example of template litteral
+    and new line  and my name ${name}
+`
+console.log(template_literals)
