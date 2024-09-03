55224501086f8d074e41f210283b47ac9105a6d8
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-09-02 22:58:29 -0700

    added more code examples to destructuring_arrays class

--- es6/destructuring_arrays.js
+++ es6/destructuring_arrays.js
@@ -5,3 +5,16 @@ let [a,b]=numbers
 console.log(a)
 console.log(b)
 console.log(numbers)
+
+//destructuring objects 
+let person ={
+    name:'abdul hasib',
+    age:234,
+    greet:function(){
+        console.log("Hello everyone!")
+    }
+}
+
+let {name,age} = person
+
+console.log(name)
